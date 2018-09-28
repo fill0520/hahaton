676b571b6872434be5c1092f7fbcdddb709e0abc
DeaMladic <32816002+DeaMladic@users.noreply.github.com>
2018-09-28 23:58:57 +0600

    Create tarif.py

---
+++ tarif.py
@@ -0,0 +1,28 @@
+
+[{ 
+	"Info": {
+      "Name":"Ozgocho",
+      "Requires":"0",
+      "PaymentDay":"-1",
+      "PaymentWeek":"-1",
+      "PaymentMonth":"-1"
+	},
+
+	"Call": {
+	  "Free":"0",
+	  "Cost":"0.02",
+	  "Limit":"3",
+	  "OutNet":"0.1"
+	},
+
+	"Internet": {
+      "Free":"0",
+	  "Cost":"0.1"
+	},
+
+	"SMS": {
+	  "Free":"0",
+	  "Cost":"1.5"
+	}
+	
+} ]
