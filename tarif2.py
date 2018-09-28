006527570da1a69eb4ed9717a9c8c15ed35f14e2
DeaMladic <32816002+DeaMladic@users.noreply.github.com>
2018-09-29 02:15:09 +0600

    Create tarif2.py

---
+++ tarif2.py
@@ -0,0 +1,32 @@
+
+[{ 
+	"Info": {
+      "Name":"Onoi",
+      "Requires":"1",
+      "PaymentDay":"2.99",
+      "Reset":"1"
+	},
+
+	"Call": {
+	  "Free":"300",
+	  "Free1":"-1",
+	  "Free2":"-1",
+	  "FreeOut":"-1",
+	  "Cost":"0.0197",
+	  "CostOut":"0.0832"
+	},
+
+	"Internet": {
+      "Free":"-1",
+	  "Cost":"0.299"
+	},
+
+	"SMS": {
+	  "Free":"5",
+	  "Free1":"-1",
+	  "Free2":"-1",
+	  "FreeOut":"-1",
+	  "Cost":"1.5"
+	}
+
+} ]
