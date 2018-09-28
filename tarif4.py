0d55d04d0125a4808025ef6a36b5622f51de5e29
DeaMladic <32816002+DeaMladic@users.noreply.github.com>
2018-09-29 02:16:15 +0600

    Create tarif4.py

---
+++ tarif4.py
@@ -0,0 +1,32 @@
+
+[{ 
+	"Info": {
+      "Name":"PerehodiNaO!95",
+      "Requires":"0",
+      "PaymentDay":"13.6",
+      "Reset":"7"
+	},
+
+	"Call": {
+	  "Free":"-1",
+	  "Free1":"1200",
+	  "Free2":"10800",
+	  "FreeOut":"300",
+	  "Cost":"0.0197",
+	  "CostOut":"0.082"
+	},
+
+	"Internet": {
+      "Free":"-1",
+	  "Cost":"0"
+	},
+
+	"SMS": {
+	  "Free":"-1",
+	  "Free1":"40",
+	  "Free2":"140",
+	  "FreeOut":"5",
+	  "Cost":"1.5"
+	}
+
+} ]
